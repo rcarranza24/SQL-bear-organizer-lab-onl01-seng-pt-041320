c3d5703387e3a1bff1baca8591eb8f308b6bdb66
Rosa Carranza <rcarranza1024@yahoo.com>
2020-06-10 22:12:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+ ("Mr. Chocolate", 10, "M", "medium brown", "easygoing", 1),
+ ("Rowdy", 6, "F", "black", "energetic", 1),
+ ("Tabitha", 12, "F", "dark brown", "funloving", 1),
+ ("Sargeant Brown", 17, "M", "black", "bossy", 0),
+ ("Melissa", 5, "F", "brown", "shy", 1),
+ ("Grinch", 18, "M", "black", "growly", 1),
+ ("Wendy", 6, "F", "blonde", "social", 1),
+ (null, 20, "M", "black", "aggressive", 0);
